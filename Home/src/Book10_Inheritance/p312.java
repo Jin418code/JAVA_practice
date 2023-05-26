5237c596309f71c7064b34e168d50208a69882e4
Jin <jin41879@gmail.com>
2023-05-26 20:25:29 +0900

    111

--- Home/src/Book10_Inheritance/p312.java
+++ Home/src/Book10_Inheritance/p312.java
@@ -2,5 +2,23 @@ package Book10_Inheritance;
 
 public class p312 {
 
-	int a = 0;
+	String name = null;
+	int age = 0;
+
+	void eat() {
+	}
+
+	void sleep() {
+	}
+}
+
+class Student extends p312 {
+	
+	
+	int studentID;
+	
+	
+	void goToSchool() {
+	}
+	
 }
